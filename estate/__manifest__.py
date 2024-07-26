668463aacccd0422a1507724209a0fd240164aff
odoo <odoo@odoo.com>
2024-07-26 02:13:47 -0700

    Server framework 101: Chapter 1-6

---
+++ estate/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Real Estate",
+
+    'summary': """
+        https://www.odoo.com/documentation/17.0/developer/tutorials/server_framework_101/02_newapp.html
+    """,
+
+    'description': """
+        https://www.odoo.com/documentation/17.0/developer/tutorials/server_framework_101/02_newapp.html
+    """,
+
+    'author': "Odoo",
+    'website': "https://www.odoo.com/",
+    'category': 'Tutorials/RealEstate',
+    'version': '0.1',
+    'application': True,
+    'installable': True,
+    'depends': ['base'],
+
+    'data': [
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
+    ],
+    'assets': {},
+    'license': 'AGPL-3'
+}
---
+++ estate/models/estate_property.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from dateutil import relativedelta
+
+from odoo import fields, models
+
+
+class Property(models.Model):
+    _name = "estate.property"
+    _description = "https://www.odoo.com/documentation/17.0/developer/tutorials/server_framework_101/03_basicmodel.html"
+
+    def _default_date_availability(self):
+        return fields.Date.today() + relativedelta.relativedelta(months=3)
+
+    name = fields.Char(required=True, string='Title')
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date(copy=False, default=_default_date_availability, string='Available From')
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
+    living_area = fields.Integer(string='Living Area (sqm)')
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer(string='Garden Area (sqm)')
+    garden_orientation = fields.Selection(
+        string='Garden Orientation',
+        selection=[('North', 'North'), ('South', 'South'), ('East', 'East'), ('West', 'West')],
+        help="garden orientation selection"
+    )
+    active = fields.Boolean(default=True)
+    state = fields.Selection(
+        string='Status',
+        selection=[
+            ('New', 'New'),
+            ('Offer Received', 'Offer Received'),
+            ('Offer Accepted', 'Offer Accepted'),
+            ('Sold', 'Sold'),
+            ('Canceled', 'Canceled'),
+        ],
+        help="state selection",
+        default='New'
+    )
